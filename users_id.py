86870b16d6eb08214df5157124c2193c6215f906
besapuz <aleksandzarinskij18772@gmail.com>
2022-11-12 15:10:24 +0300

    получил список id для всех участников чата

--- users_id.py
+++ users_id.py
@@ -1,26 +1,27 @@
 import asyncio
 import json
 from telethon.sync import TelegramClient
-
+from os import environ
+from dotenv import load_dotenv
 # классы для работы с каналами
 from telethon.tl.functions.channels import GetParticipantsRequest
 from telethon.tl.types import ChannelParticipantsSearch
 
+# Получение переменных окружения
+load_dotenv()
 
 # Присваиваем значения внутренним переменным
-api_id = en
-api_hash = "811b9f8a786dfd2b4d706d0cc8b6d07d"
-username = "Alekzary"
-
-# proxy = (proxy_server, 3128, proxy_key)
+api_id = environ["API_ID"]
+api_hash = environ["API_HASH"]
+username = environ["USER_NAME"]
 
 client = TelegramClient(username, api_id, api_hash)
 
 client.start()
 
 
-async def dump_all_participants(channel):
-    """Записывает json-файл с информацией о всех участниках канала/чата"""
+async def get_user_id(channel) -> None:
+    """Получает id всех пользователей канала"""
     offset_user = 0  # номер участника, с которого начинается считывание
     limit_user = 100  # максимальное число записей, передаваемых за один раз
 
@@ -35,24 +36,18 @@ async def dump_all_participants(channel):
         all_participants.extend(participants.users)
         offset_user += len(participants.users)
 
-    all_users_details = []  # список словарей с интересующими параметрами участников канала
+    all_users_details = set() # список telegram id каждого пользователя
 
     for participant in all_participants:
-        all_users_details.append({"id": participant.id,
-                                  "first_name": participant.first_name,
-                                  "last_name": participant.last_name,
-                                  "user": participant.username,
-                                  "phone": participant.phone,
-                                  "is_bot": participant.bot})
+        all_users_details.add(participant.id)
 
-    with open('channel_users.json', 'w', encoding='utf8') as outfile:
-        json.dump(all_users_details, outfile, ensure_ascii=False)
+#     дописать сюда проверку
 
 
 async def main():
-    url = input("Введите ссылку на канал или чат: ")
+    url = "https://t.me/+EMixgInnJWdlODU6"
     channel = await client.get_entity(url)
-    await dump_all_participants(channel)
+    await get_user_id(channel)
 
 
 with client:
